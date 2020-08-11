cd3e97106a3895514ffcf256e1d2e35f0d79eed7
kamifubuki <csjinjay@qq.com>
2020-08-11 14:07:17 +0800

    break outer loop

---
+++ amazing java grammar/src/outerLoop.java
@@ -0,0 +1,16 @@
+
+public class outerLoop {
+	public static void main(String[] args) {
+		stop:for(int i = 0; i < 5 ; i++) {
+			
+			for(int j = 0; j < 5 ; j++) {
+				if(j==3)
+					break stop;
+				System.out.println("j="+j);
+			}
+			System.out.println("outerloop end");
+		}
+		System.out.println("end");
+	}
+	
+}
