17d6ac6e0c212266af4ac628ad043156ce467581
kamifubuki <csjinjay@qq.com>
2020-08-11 13:33:40 +0800

    内循环break 外循环停下

--- whoami/src/whoami/Persona.java
+++ whoami/src/whoami/Persona.java
@@ -5,10 +5,24 @@ public class Persona {
 	float attack=0;
 	int hp=0;
 	String skillName;
+	
+	public Persona() {
+		
+	}
+	public Persona(String name,int hp) {
+		this.name = name;
+		this.hp = hp;
+	}
+	
 	void attack() {
 		System.out.println("normal attack");
 	}
 	void persona() {
 		System.out.println("skill attack");
 	}
+	public void heal(int heal_item) {
+		hp = hp + heal_item;
+		heal_item = 0 ;
+	}
+	
 }
--- whoami/src/whoami/whatisthis.java
+++ whoami/src/whoami/whatisthis.java
@@ -3,8 +3,14 @@ package whoami;
 public class whatisthis {
 	
 	public static void main(String[] args) {
-		Persona izanagi=new Persona();
-		System.out.println(izanagi);
+		Persona Ars¨¨ne=new Persona("ÑÇÉ­",100);
+		System.out.println(Ars¨¨ne.hp);
+		int heal_item=20;
+		Ars¨¨ne.heal(heal_item);
+		System.out.println(Ars¨¨ne.hp);
+		System.out.println(heal_item);
+		
+		
 		System.out.println("end");
 	}
 }
